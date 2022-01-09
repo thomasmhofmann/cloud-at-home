2fdfc33cad68af6caf25a3cc75a7f3ce474402d4
Thomas Hofmann <email@thomashofmann.com>
2022-01-09 22:03:58 +0100

    custom backend for ingress no self registration for gitea

---
+++ ingress-nginx/install.sh
@@ -0,0 +1,3 @@
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm repo update
+helm upgrade --install ingress-nginx -n ingress-nginx ingress-nginx/ingress-nginx -f values.yaml
